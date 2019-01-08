c4de8c9f15e85c73f5dc8b04d82f5d9d86a8fe23
Aaron Lafrentz <arlafrentz@gmail.com>
2019-01-08 10:53:09 -0500

    first commit

---
+++ 01-grammarAndType/2-declarations.js
@@ -0,0 +1,47 @@
+var a = 1;
+
+var     b      =        1;
+/* (1)   (2)    (3)      (4)
+
+1- Keyword
+2- Variable name
+3- Assignment opperator
+4- Variable value
+
+*/
+console.log(a + b);
+
+
+
+
+/***************************
+ DECLARATIONS
+ ***************************/   
+
+
+var x;
+console.log('Declaration:', x); // Declaration: undefined
+
+x = 10;
+console.log('Initialization 1:', x); // Initialization 1: 10
+
+x = 33;
+console.log('Initialization 2:', x); // Initialization 2: 33
+
+var y = 'Hello';
+console.log('Both:',x, y); // Both: 33 hello
+
+
+
+Collapse 
+
+ /*
+ Var, Let, and Const:
+ */
+let today = 'Great!';
+const elevenFifty = 'wounderful!';
+console.log(today, elevenFifty)
+ 
+
+today = "lovely!";
+console.log(today, elevenFifty)
